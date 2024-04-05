5c7dcf2b01eb7fa2964f81470a99c9932215a907
Mor-gan <ogundipeabimbola9@gmail.com>
2024-04-05 11:29:30 +0100

    commit get all payment function

--- src/main/java/com/payment/payment/PaymentServces.java
+++ src/main/java/com/payment/payment/PaymentServces.java
@@ -1,13 +1,32 @@
 package com.payment.payment;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.io.File;
 import java.util.UUID;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public interface PaymentServces {
 
-     static List<Payment> getAllPayments() {
-      return null;
+    String filePath = "/Users/bimbo/Documents/payment/src/main/resources/paymentdetails.json";
+
+     static List<Payment> getAllPayments(String filePath) {
+        List<Payment> payments = new ArrayList<>();
+        try {
+            File sourceFile = Paths.get(filePath).toFile();
+            ObjectMapper objectMapper = new ObjectMapper();
+            payments = Arrays.asList(objectMapper.readValue(sourceFile, Payment[].class));
+            payments.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } return payments;
    } 
+
+
    static boolean cancelPayment(UUID id) {
       return true;
 
