4fd1f801c8cde584ba887f17fed921758e69f7cc
Mor-gan <ogundipeabimbola9@gmail.com>
2024-05-08 12:36:37 +0100

    commit this

--- src/main/java/com/payment/payment/listofpayment/CardPayment.java
+++ src/main/java/com/payment/payment/listofpayment/CardPayment.java
@@ -1,31 +1,27 @@
-// package com.payment.payment.listofpayment;
+package com.payment.payment.listofpayment;
 
-// import java.math.BigDecimal;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// import com.payment.payment.Payment;
-// import com.payment.payment.PaymentServces;
-// import com.payment.payment.filehandler.InsufficientBalanceException;
+import com.payment.payment.Payment;
+import com.payment.payment.PaymentServces;
 
-// @Service
-// public class CardPayment extends PaymentServces {
 
-//     private List<Payment> payments = new ArrayList<>();
-     
+@Service
+public class CardPayment extends PaymentServces {
 
-//     public Payment processPayment(Payment createPayment) throws InsufficientBalanceException {
-//         BigDecimal paymentAmount = createPayment.getAmount();
-//         BigDecimal cardBalance = createPayment.getBalance();
-//         if (cardBalance.compareTo(paymentAmount) >= 0) {
-//             payments.add(createPayment);
-//         } else {
-//             throw new InsufficientBalanceException("Insufficient Payment amount");
-//         }
-//         return createPayment; // Return the created Payment on success
-//     }
+    private List<Payment> payments = new ArrayList<>();
+    
+    public Payment processPayment(Payment createPayment)  {
+        BigDecimal paymentAmount = createPayment.getAmount();
+        BigDecimal cardBalance = createPayment.getBalance();
+        if (cardBalance.compareTo(paymentAmount) >= 0) {
+            payments.add(createPayment);
+        }return createPayment; // Return the created Payment on success
+    }
 
-// }
+}
 
